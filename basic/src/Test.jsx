40fa5495b9bd0eaf4629f4b67277972c68c91d66
cheeky-2 <jjuing0@gmail.com>
2025-06-20 18:12:44 +0900

    리액트 컴포넌트 공부

--- basic/src/Test.jsx
+++ basic/src/Test.jsx
@@ -1,9 +1,11 @@
 import Header from "./Header.jsx";
-/* 컴포넌트 생성 */
+import './Test.css';
+/* 컴포넌트 생성 Test(부모컴포넌트) */
 function Test(){
     return (
         <>
             <Header />
+            <Aside />
             <h1>Test 컴포넌트</h1>
         </>
     )
@@ -11,3 +13,17 @@ function Test(){
 
 /* 다른컴포넌트에서 해당 Test컴포넌트 사용가능하도록 내보내기 설정 */
 export default Test;
+function Aside(){
+    const leftNav = document.querySelector('div');
+    leftNav.classList.add('left_nav');
+    return(
+        <aside>
+            <div>왼쪽네비
+                <a href="#" target="_blank">메인화면으로</a>
+            </div>
+            <h1 style={{borderBottom:'1px solid #f00'}}>서브메뉴</h1>
+            <a href="#">메뉴</a>
+            <hr />
+        </aside>
+    )
+}
